639e0aaceed4ed86217638c820340ca10f7c6a60
Ren Saito <ren.saito.dev@gmail.com>
2025-04-07 22:13:10 +1200

    add location widget to taskbar

--- src/components/DesktopShell.tsx
+++ src/components/DesktopShell.tsx
@@ -1,4 +1,5 @@
 import styles from './DesktopShell.module.css'
+import LocationIcon from '../assets/location-icon.svg?react'
 
 const DesktopShell = () => {
   return (
@@ -21,6 +22,16 @@ const Taskbar = () => {
   return (
     <div className={styles.taskbar}>
       <p>Taskbar</p>
+      <TaskbarLocation />
+    </div>
+  )
+}
+
+const TaskbarLocation = () => {
+  return (
+    <div className={styles.taskbar_location}>
+      <LocationIcon className={styles.taskbar_location_icon} stroke="white" />
+      <p>Auckland, New Zealand</p>
     </div>
   )
 }
