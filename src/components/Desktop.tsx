99d8caab4b2efe38c57efcfaaac9c4cfeaba7ebe
Ren Saito <ren.saito.dev@gmail.com>
2025-04-09 14:54:28 +1200

    implement Desktop layout with background, grid and dynamic item positioning

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,9 +1,27 @@
 import styles from './Desktop.module.css'
+import DesktopItem from './DesktopItem'
 
 const Desktop = () => {
   return (
     <div className={styles.desktop}>
-      <p>Desktop</p>
+      <DesktopBackground />
+      <DesktopGrid />
+    </div>
+  )
+}
+
+const DesktopBackground = () => {
+  return (
+    <div className={styles.desktopBackground}>
+      <p>Desktop Background</p>
+    </div>
+  )
+}
+
+const DesktopGrid = () => {
+  return (
+    <div className={styles.desktopGrid}>
+      <DesktopItem row={4} column={7} />
     </div>
   )
 }
---
+++ src/components/DesktopItem.tsx
@@ -0,0 +1,20 @@
+import styles from './DesktopItem.module.css'
+import { CSSProperties } from 'react'
+
+interface DesktopItemProps {
+  row: number
+  column: number
+}
+const DesktopItem = (props: DesktopItemProps) => {
+  const { row, column } = props
+  const style: CSSProperties = { gridRow: row, gridColumn: column }
+  return (
+    <div className={styles.desktopItem} style={style}>
+      <p>Desktop Item</p>
+      <p>row: {row}</p>
+      <p>column: {column}</p>
+    </div>
+  )
+}
+
+export default DesktopItem
