8c5f1d86b1a37b570426d07387ed7881ee34c471
Ren Saito <ren.saito.dev@gmail.com>
2025-04-12 22:08:05 +1200

    add optional shortcut prop to DesktopItem interface, add Source Code item

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import DesktopShell from 'src/components/DesktopShell'
 import { DesktopItemProps } from 'src/components/DesktopItem'
 import ComputerIcon from 'src/assets/icons/computer_explorer.png'
+import InternetIcon from 'src/assets/icons/internet_connection_wiz.png'
 
 function App() {
   const desktopItems: DesktopItemProps[] = [
     { row: 1, col: 1, title: 'Computer', icon: ComputerIcon },
+    {
+      row: 2,
+      col: 1,
+      title: 'Source Code',
+      icon: InternetIcon,
+      shortcut: true,
+    },
   ]
   return <DesktopShell items={desktopItems} />
 }
