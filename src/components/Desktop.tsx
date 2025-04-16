ad74993d0dea38fe9cc4aa283bb70ce791d71c22
Ren Saito <ren.saito.dev@gmail.com>
2025-04-16 19:34:20 +1200

    add back DesktopItems component

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -4,9 +4,17 @@ import DesktopItem from 'src/components/DesktopItem'
 import AppContext from 'src/contexts/AppContext'
 
 const Desktop = () => {
-  const { desktopItems } = useContext(AppContext)
   return (
     <div className={styles.desktop}>
+      <DesktopItems />
+    </div>
+  )
+}
+
+const DesktopItems = () => {
+  const { desktopItems } = useContext(AppContext)
+  return (
+    <div className={styles.desktopItems}>
       {desktopItems.map((item, index) => (
         <DesktopItem key={index} {...item} />
       ))}
