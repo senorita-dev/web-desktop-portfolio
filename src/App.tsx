257418a848fec796d4a0d59a898ab1405ed721ed
Ren Saito <ren.saito.dev@gmail.com>
2025-04-19 15:44:26 +1200

    set up react-redux reducers and slices for desktopIcons and windows

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import '98.css'
-import AppContext, { appContext } from 'src/contexts/AppContext'
+import { Provider } from 'react-redux'
 import Desktop from 'src/components/Desktop'
 import Taskbar from 'src/components/Taskbar'
+import AppContext, { appContext } from 'src/contexts/AppContext'
+import store from 'src/redux/store'
 
 function App() {
   return (
-    <AppContext.Provider value={appContext}>
-      <Desktop />
-      <Taskbar />
-    </AppContext.Provider>
+    <Provider store={store}>
+      <AppContext.Provider value={appContext}>
+        <Desktop />
+        <Taskbar />
+      </AppContext.Provider>
+    </Provider>
   )
 }
 
---
+++ src/redux/slices/desktopIconsSlice.ts
@@ -0,0 +1,76 @@
+import { createSlice } from '@reduxjs/toolkit'
+import ComputerIcon from 'src/assets/icons/computer_explorer.png'
+import InternetIcon from 'src/assets/icons/internet_connection_wiz.png'
+import NotepadIcon from 'src/assets/icons/notepad.png'
+import { DesktopIconProps } from 'src/components/DesktopIcon'
+
+interface DesktopIconsState {
+  value: DesktopIconProps[]
+}
+
+const initialState: DesktopIconsState = {
+  value: [
+    {
+      row: 1,
+      col: 1,
+      title: 'Computer',
+      icon: ComputerIcon,
+    },
+    {
+      row: 2,
+      col: 1,
+      title: 'Source Code',
+      icon: InternetIcon,
+      onOpen: () => {
+        window.open(
+          'https://github.com/senorita-dev/web-desktop-portfolio',
+          '_blank',
+          'noopener,noreferrer',
+        )
+      },
+      shortcut: true,
+    },
+    {
+      row: 3,
+      col: 1,
+      title: 'GitHub',
+      icon: InternetIcon,
+      onOpen: () => {
+        window.open(
+          'https://github.com/senorita-dev',
+          '_blank',
+          'noopener,noreferrer',
+        )
+      },
+      shortcut: true,
+    },
+    {
+      row: 4,
+      col: 1,
+      title: 'LinkedIn',
+      icon: InternetIcon,
+      onOpen: () => {
+        window.open(
+          'https://www.linkedin.com/in/ren-saito/',
+          '_blank',
+          'noopener,noreferrer',
+        )
+      },
+      shortcut: true,
+    },
+    {
+      row: 5,
+      col: 1,
+      title: 'weaknesses',
+      icon: NotepadIcon,
+    },
+  ],
+}
+
+const desktopIconsSlice = createSlice({
+  name: 'desktopIcons',
+  initialState,
+  reducers: {},
+})
+
+export default desktopIconsSlice.reducer
