784c5c8c0ee812106fe2df2cb702582adf261d93
Ren Saito <ren.saito.dev@gmail.com>
2025-04-21 20:04:25 +1200

    add taskbar window icons

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -1,11 +1,14 @@
 import styles from 'src/components/Taskbar.module.css'
 import { useEffect, useRef, useState } from 'react'
 import WindowsIcon from 'src/assets/icons/windows.png'
+import { useAppSelector } from 'src/redux/hooks'
+import { WindowProps } from './Window'
 
 const Taskbar = () => {
   return (
     <div className={styles.taskbar}>
       <TaskbarStartButton />
+      <TaskbarWindowIcons />
       <TaskbarDateTime />
     </div>
   )
@@ -17,6 +20,32 @@ const TaskbarStartButton = () => {
       <img src={WindowsIcon} className={styles.taskbar_startButton_icon} />
       <span>Start</span>
     </div>
+  )
+}
+
+const TaskbarWindowIcons = () => {
+  const state = useAppSelector((state) => state.windows)
+  const { windows } = state
+  return (
+    <div className={styles.taskbar_windowIcons}>
+      {windows.map((window) => (
+        <TaskbarWindowIcon key={window.id} {...window} />
+      ))}
+    </div>
+  )
+}
+
+type TaskbarWindowIconProps = WindowProps
+const TaskbarWindowIcon = (props: TaskbarWindowIconProps) => {
+  const { file } = props
+  const title = `${file.title} - ${file.applicationType}`
+  return (
+    <>
+      <div className={`${styles.taskbar_item} ${styles.taskbar_windowIcon}`}>
+        <img src={file.icon} className={styles.taskbar_windowIcon_icon} />
+        <span className={styles.taskbar_windowIcon_text}>{title}</span>
+      </div>
+    </>
   )
 }
 
