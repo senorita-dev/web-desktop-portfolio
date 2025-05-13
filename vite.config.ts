c6c103595d91bf394603fdfce6b96db7a5ed56fe
Ren Saito <ren.saito.dev@gmail.com>
2025-05-13 23:09:24 +1200

    add environment variable VITE_BASE_URL to set vite base

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      src: '/src',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.VITE_BASE_URL ?? '/',
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        src: '/src',
+      },
     },
-  },
+  }
 })
